94c9b5397f235d56a196849a85e5e534bc1f6782
RequestOutofRange <jlinnealundberg@yahoo.se>
2023-01-23 21:37:46 +0100

    text inlagd

---
+++ task_1_1/Program.cs
@@ -0,0 +1,6 @@
+﻿/*
+ * Skriv ett program som simulerar 10 000 kast med en sexsidig tärning.  Programmet skall efteråt 
+skriva ut hur många av tärningskasten som gav två respektive fem.  Använd ett fält med en lämplig 
+storlek för att lösa uppgiften. */
+
+
---
+++ task_1_2/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * När du handlar i en affär och betalar kontant får du kanske växel tillbaka. I regel får affärsbiträdet 
+hjälp av kassaapparaten med att beräkna summan man ska få tillbaka, men inte alltid vilka sedlar 
+och  mynt  som  ska  lämnas  tillbaka.  Skriv  ett  program  som  beräknar  den  växel  biträdet  ska  ge 
+tillbaka i samband med ett köp. Programmet ska, förutom att presentera beloppet kunden får tillbaka 
+avrundat till närmsta 50-öring, även bestämma vilka, och antalet, sedlar och mynt. Kunden ska få 
+så få sedlar och mynt som möjligt tillbaka. Programmet ska kunna ge växel tillbaka med sedlar av 
+valörerna 1000, 500, 200, 100, 50 och 20 samt mynten 10, 5, 1 och 50-öring. Du kan anta att det 
+alltid finns tillräckligt antal av de sedlar och mynt som krävs. 
+ 
+Skriv endast ut de valörer som ska lämnas tillbaka. Eventuella fel i samband med inmatningen ska 
+tas om hand. Använd try/catch och fånga lämpligt undantag (exception). Glöm inte att testa 
+programmet. Om t.ex. en kund handlar för 371,38 kr avrundas beloppet till 371,50 kr. Om kunden 
+betalar med en 1000 kr-sedel ska kunden få 628,50 kr tillbaka, fördelat enligt figuren.
+*/
