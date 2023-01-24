c1458ae8e15bebfedd0d85f6abd07e6b94ce3fbf
adrian <adreli@student.chalmers.se>
2023-01-24 19:07:11 +0100

    Added userInputLib

---
+++ UserInputFunctions/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace UserInputFunctions
+{
+    public class UserInputFunctions
+    {
+        static public void ReadInt(string q, out int output)
+        {
+            Console.Write(q);
+            while (!int.TryParse(Console.ReadLine(), out output))
+            {
+                Console.WriteLine("Expected integer, please try your input again!");
+                Console.WriteLine();
+                Console.WriteLine(q);
+            };
+        }
+    }
+}
---
+++ task1_5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static UserInputFunctions.UserInputFunctions;
+
+namespace bowling
+{
+    internal class Bowling
+    {
+        static void Main(string[] args)
+        {
+            int outs;
+            ReadInt("Gimme int: ", out outs);
+            Console.WriteLine($"You wrote {outs}");
+        }
+    }
+}
+
