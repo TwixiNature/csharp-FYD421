f6c9d261e693f67f9b230fee101fcb551c13504b
RequestOutofRange <jlinnealundberg@yahoo.se>
2023-02-04 12:39:25 +0100

    ändringar från lektionen

--- task1_5/Program.cs
+++ task1_5/Program.cs
@@ -13,90 +13,111 @@ namespace bowling
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome, lets play bowling!");
+            int trashBin = 0;
             int doubleScore = 0; // keeping track of combo from previous spare/strike
-            int totalScore = 0; //total score of player
-            char[] Total = { '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', };  //score throughout the game
+            int[] Total = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };  //score throughout the game
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine($"Round {i+1}: throw 1");
+                Console.WriteLine($"Round {i + 1}: throw 1");
                 int roundScore = 0;
-                int pinsHit2 = 0; 
+                int pinsHit2 = 0;
                 int pinsHit = PinsHit(); // get nr pins hit on first throw
-                roundScore += GetScore(pinsHit, 0, ref doubleScore);
+                if (i == 0)
+                {
+                    roundScore += GetScore(pinsHit, -1, ref doubleScore, ref trashBin);
+                }
+                else
+                {
+                    roundScore += GetScore(pinsHit, -1, ref doubleScore, ref Total[i - 1]);
+                }
                 if (pinsHit < 10) // No strike on first throw, allow second throw
                 {
-                    Console.WriteLine($"Round {i+1}: throw 2");
+                    Console.WriteLine($"Round {i + 1}: throw 2");
                     pinsHit2 = PinsHit(pinsHit); // get nr pins hit on second throw
-                    roundScore += GetScore(pinsHit2, pinsHit, ref doubleScore);
+                    if (i == 0)
+                    {
+                        roundScore += GetScore(pinsHit2, pinsHit, ref doubleScore, ref trashBin);
+                    }
+                    else
+                    {
+                        roundScore += GetScore(pinsHit2, pinsHit, ref doubleScore, ref Total[i - 1]);
+                    }
                 }
                 Console.WriteLine("This round u score: " + roundScore);
 
 
                 if (i == 9) // Final round
                 {
-                    if(pinsHit == 10) {
+                    if (pinsHit == 10)
+                    {
                         Console.WriteLine("Strike on the last round, u get 2 extra shots");
                         int hit1 = PinsHit();
-                        roundScore += GetScore(hit1, 0, ref doubleScore);
-                        int hit2 = PinsHit(hit1);
-                        roundScore += GetScore(hit2, hit1, ref doubleScore);
+                        roundScore += GetScore(hit1, -1, ref doubleScore, ref Total[i]);
 
-                    } else if (IsSpare(pinsHit, pinsHit2)) {
+                        int hit2 = PinsHit(hit1, true);
+                        roundScore += GetScore(hit2, hit1, ref doubleScore, ref Total[i]);
+
+                    }
+                    else if (IsSpare(pinsHit, pinsHit2))
+                    {
                         Console.WriteLine("Spare on the last round, u get 1 extra shot");
                         int hit2 = PinsHit();
-                        roundScore += GetScore(hit2, 0, ref doubleScore);
+                        roundScore += GetScore(hit2, -1, ref doubleScore, ref Total[i]);
                     }
-                    totalScore += roundScore;
                 }
-                else { totalScore += roundScore; }
+                Total[i] = roundScore;
+
+                // printing scorecard
+                Console.Write("Score so far [ ");
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write($" {Total[j]} ");
+                }
+                Console.WriteLine(" ]");
             }
-            Console.WriteLine("Your score was in total: " + totalScore);
+            Console.WriteLine("Your score was in total: " + Total.Sum());
         }
 
         static bool IsSpare(int hit1, int hit2) //
         {
             return hit1 < 10 && hit2 + hit1 == 10;
         }
-        static int PinsHit(int prevPins = 0) 
+        static int PinsHit(int prevPins = 0, bool allowit = false)
         {
             int pins = 0;
             do
             {
-                ReadInt("How many pins were knocked?: ", out  pins);
-                if (pins > 10 || pins < 0 || (pins + prevPins) > 10)
+                ReadInt("How many pins were knocked?: ", out pins);
+                if ((pins > 10 || pins < 0 || (pins + prevPins) > 10) && !allowit)
                 {
                     Console.WriteLine("You cannot hit more than 10 or negative number of pins, try again");
                 }
-            } while (pins > 10 || pins < 0); //cannot hit more than 10 or negative number of pins
-            
+            } while ((pins > 10 || pins < 0 || (pins + prevPins) > 10) && !allowit); //cannot hit more than 10 or negative number of pins
+
 
             return pins;
         }
-        static int GetScore(int pins, int carry, ref int doubleCount)
+        static int GetScore(int pins, int carry, ref int doubleCount, ref int prevScore)
         { // ( pins knocked down, carry over points, strike/spare in previous round )
-            int score = 0;
+
             if (doubleCount > 0) // if points combine from previous strike or spare
             {
-                score += pins * 2;
+                prevScore += pins; // add pins fromcurrent throw to strike/spare score
                 doubleCount--;
             }
-            else
-            {
-                score += pins;
-            }
 
-            if (pins == 10) 
+            if (pins == 10 && carry == -1)
             {
                 Console.WriteLine("Strike!");
                 doubleCount = 2;
             }
-            if(IsSpare(carry, pins))
+            else if (IsSpare(carry, pins))
             {
                 Console.WriteLine("Spare!");
                 doubleCount = 1;
             }
 
-            return score;
+            return pins;
         }
     }
 
--- task_1_8/Program.cs
+++ task_1_8/Program.cs
@@ -15,31 +15,30 @@ namespace calculation
 
         static void Main(string[] args)
         {
-            var integral = new Integral_calc(); //create object
-            int a , b, N;
-            double n, sum = 0 ; 
-            ReadInt("Set start of integral, a= ?", out a); //set start of integral
-            ReadInt("Set end of integral, b= ?", out b); //set end of integral
+            int N;
+            double a, b, n, sum = 0;
+            a = tryReadDouble("Set start of integral, a= ?"); //set start of integral
+            b = tryReadDouble("Set end of integral, b= ?"); //set end of integral
             ReadInt("Set number of sections, N= ?", out N); //set number of sections
 
-            
-            n = (b-a)/(double)N; //width of rectangl
+
+            n = (b - a) / N; //width of rectangl
 
 
-            for (double x =a; x <b; x =x + n){
-                double y = integral.get_func_val(x+ n/2); // measures height at middle of rectangle
+            for (double x = a; x < b; x = x + n)
+            {
+                double y = get_func_val(x + n / 2); // measures height at middle of rectangle
                 sum = sum + y * n;  // height * width of rectangle
             }
 
             Console.WriteLine($"The integral is {sum}");
-            
+
         }
 
-        double get_func_val ( double x) // returns value of integral function at x
+        static double get_func_val(double x) // returns value of integral function at x
         {
-            double f =  x; //4 * Math.Pow(x, 2) + 3; // 4x^2 +3
-
-            return f; 
+            double f = 4 * Math.Pow(x, 2) + 3; // 4x^2 +3
+            return f;
         }
-    } 
+    }
 }
