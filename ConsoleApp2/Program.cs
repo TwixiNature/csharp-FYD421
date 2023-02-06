004d37be8dad681f853db41b835dca19e60f84c5
adrian <adreli@student.chalmers.se>
2023-02-06 21:35:53 +0100

    Add 2.5

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Sorting
+{
+ 
+    internal class Student : IComparable<Student>
+    {
+        private int age;
+        private string name;
+        public Student(string name, int age) { 
+            this.name = name;
+            this.age = age;
+        }
+        static void Main(string[] args)
+        {
+            List<Student> list = new List<Student>();
+            list.Add(new Student("Badrian", 3));
+            list.Add(new Student("Cadrian", 1));
+            list.Add(new Student("Adrian", 2));
+            list.Add(new Student("Badrian", -2));
+
+            list.ForEach(student => Console.WriteLine(student.name + " " + student.age));
+            Console.WriteLine();
+            list.Sort();
+            list.ForEach(student => Console.WriteLine(student.name + " " + student.age));
+        }
+
+        public int CompareTo(Student? other)
+        {
+            if (other == null)
+            {
+                return 0;
+            }
+            if (other.name == this.name)
+            {
+                return this.age.CompareTo(other.age); ;
+
+            }
+            return this.name.CompareTo(other.name);
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+
+namespace Sorting
+{
+
+    internal class Student : IComparable<Student>
+    {
+        private int age;
+        private string name;
+        public Student(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public class CompareName : IComparer<Student>
+        {
+            public int Compare(Student? x, Student? y)
+            {
+                if (x == null && y == null) return 0;
+                if (x == null || y == null) { return 1; }
+
+                return (new CaseInsensitiveComparer()).Compare(x.name, y.name);
+            }
+        }
+        public class CompareAge : IComparer<Student>
+        {
+            public int Compare(Student? x, Student? y)
+            {
+                if (x == null && y == null) return 0;
+                if (x == null || y == null) { return 1; }
+
+                return (new CaseInsensitiveComparer()).Compare(x.age, y.age);
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<Student> list = new List<Student>();
+            list.Add(new Student("Badrian", 3));
+            list.Add(new Student("Cadrian", 1));
+            list.Add(new Student("Adrian", 2));
+            list.Add(new Student("Badrian", -2));
+
+            list.ForEach(student => Console.WriteLine(student.name + " " + student.age));
+            Console.WriteLine();
+            list.Sort();
+            list.ForEach(student => Console.WriteLine(student.name + " " + student.age));
+            Console.WriteLine();
+            list.Sort(new CompareAge());
+            list.ForEach(student => Console.WriteLine(student.name + " " + student.age));
+            Console.WriteLine();
+            list.Sort(new CompareName());
+            list.ForEach(student => Console.WriteLine(student.name + " " + student.age));
+            
+        }
+
+        public int CompareTo(Student? other)
+        {
+            if (other == null)
+            {
+                return 0;
+            }
+            if (other.name == this.name)
+            {
+                return this.age.CompareTo(other.age); ;
+
+            }
+            return this.name.CompareTo(other.name);
+        }
+    }
+}
