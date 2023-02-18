c16143ce6520a46b4511a5e4db721f8c3f5fb8fa
adrian <adreli@student.chalmers.se>
2023-02-18 17:38:10 +0100

    Add solution file changes

--- task_3_10/Program.cs
+++ task_3_10/Program.cs
@@ -14,6 +14,7 @@ namespace task_3_10
         [STAThread]
         static void Main()
         {
+            Console.WriteLine("hej");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
